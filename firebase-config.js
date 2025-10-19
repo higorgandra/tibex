fdfdb2aa71f4bd214619fdc3653bb612b714c14e
higorgandra <112503688+higorgandra@users.noreply.github.com>
2025-10-19 09:26:11 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// c:\Users\Vitoria\Desktop\tibex\firebase-config.js
+
+// Este arquivo contém a configuração do Firebase.
+// Em um ambiente de produção real com controle de versão (Git),
+// este arquivo seria adicionado ao .gitignore para não expor as chaves no repositório.
+const firebaseConfig = {
+  apiKey: "AIzaSyBZMKACtQClHgV9_4hAwe4npKPwgBi_3ew",
+  authDomain: "tibex-com-br.firebaseapp.com",
+  projectId: "tibex-com-br",
+  storageBucket: "tibex-com-br.appspot.com",
+  messagingSenderId: "548414150725",
+  appId: "1:548414150725:web:f7e1cc92b24205da582b2c",
+  measurementId: "G-Q1WY2T1C5K"
+};
