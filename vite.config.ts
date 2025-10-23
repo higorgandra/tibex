8ff091daa20405e7d801aa5f232ba20ee23181d4
Higor <higorgandra@gmail.com>
2025-10-23 00:48:30 -0300

    fix: Adiciona dependência do rollup para build em Linux

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tibex/', // Use o nome do seu repositório aqui!
+  base: '/',
   plugins: [react()],
 })
-
-
